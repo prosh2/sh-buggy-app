c2001c60c62943fc0b281982b3501629e924dd93
yzhedwin <zizo_edwin@hotmail.com>
2025-11-16 17:35:39 +0800

    doc: update manifest file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,28 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Buggy",
-  description: "Split your bills with ease.",
+  description:
+    "Buggy makes splitting bills with friends simple. Snap a photo of the bill,settle balances instantly, and avoid awkward money chats. Try Buggy today!",
+  keywords: [
+    "take picture and split bill",
+    "snap a picture",
+    "split bill",
+    "bill splitting",
+    "bill splitting app",
+    "split bills with friends",
+    "group expenses app",
+    "expense sharing",
+    "roommate expenses",
+    "travel expense tracker",
+    "split payment app",
+    "bill splitter app",
+    "ocr text extraction",
+    "deepseek ocr",
+    "ai ocr",
+    "receipt ocr",
+    "split bill app",
+  ],
+  metadataBase: new URL("https://buggy.prosh2.com"),
 };
 
 export default function RootLayout({
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,7 +4,8 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Buggy by prosh2",
     short_name: "Buggy",
-    description: "Buggy - Split your bills with friends easily!",
+    description:
+      "Buggy makes splitting bills with friends simple. Snap a photo of the bill,settle balances instantly, and avoid awkward money chats. Try Buggy today!",
     start_url: "/",
     display: "standalone",
     background_color: "oklch(21% .034 264.665)",
